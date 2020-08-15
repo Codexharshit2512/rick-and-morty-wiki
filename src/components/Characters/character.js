3336e69dea12a0b894ea14bba5ae3a682700070e
karanb1 <balodikanu124@gmail.com>
2020-08-15 22:58:12 +0530

    cards

---
+++ src/components/Characters/character.js
@@ -0,0 +1,17 @@
+import rick from "../../images/rick.jpg";
+import morty from "../../images/morty.jpg";
+const character = [
+  {
+    name: "Rick Sanchez",
+    description: `Some quick example text to build on the card title and make up the bulk of the card's content.`,
+    image: rick,
+  },
+  {
+    name: "Morty Smith",
+    description: `Some quick example text to build on the card title and make up the bulk of the card's content.`,
+    image: morty,
+  },
+  {},
+];
+
+export default character;
