0465e429eff21dbf4b3f26580b83d1826db1e476
Harshit Jain <jain2008.harshu@gmail.com>
2020-08-21 00:08:23 +0530

    Updates

---
+++ precache-manifest.463ae0c94df1c5643ed898a449a5a917.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81dff9ced70af75628e4e6b574acbe3",
+    "url": "/rick-and-morty-wiki/index.html"
+  },
+  {
+    "revision": "7ff99204c10f0e2c04ee",
+    "url": "/rick-and-morty-wiki/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0a405cc9c21a9a66d07a",
+    "url": "/rick-and-morty-wiki/static/css/main.cb4dba17.chunk.css"
+  },
+  {
+    "revision": "7ff99204c10f0e2c04ee",
+    "url": "/rick-and-morty-wiki/static/js/2.943b17ab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rick-and-morty-wiki/static/js/2.943b17ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a405cc9c21a9a66d07a",
+    "url": "/rick-and-morty-wiki/static/js/main.861c4e32.chunk.js"
+  },
+  {
+    "revision": "934c01adb03e3a34aa33",
+    "url": "/rick-and-morty-wiki/static/js/runtime-main.6559e6db.js"
+  },
+  {
+    "revision": "4ea7fb833c63a4ea0b0fb696c5919dd7",
+    "url": "/rick-and-morty-wiki/static/media/bg.4ea7fb83.png"
+  },
+  {
+    "revision": "eab637076b22c782edf0015a0ec3c686",
+    "url": "/rick-and-morty-wiki/static/media/logo.eab63707.png"
+  },
+  {
+    "revision": "577f750a4ad722aed70ed4c8c669630e",
+    "url": "/rick-and-morty-wiki/static/media/morty.577f750a.jpg"
+  },
+  {
+    "revision": "8d301d243c5d767e8ff8316f69e4ea2b",
+    "url": "/rick-and-morty-wiki/static/media/poopy.8d301d24.png"
+  },
+  {
+    "revision": "499237c9f3ea1f5a6ef2e9997dbd884c",
+    "url": "/rick-and-morty-wiki/static/media/rick.499237c9.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rick-and-morty-wiki/precache-manifest.463ae0c94df1c5643ed898a449a5a917.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rick-and-morty-wiki/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
