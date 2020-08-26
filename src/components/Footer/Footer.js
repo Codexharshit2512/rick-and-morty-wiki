ae71ad04199b274d44c9f989c91b5afb5f62133a
Mananx23 <manan.olympian23@gmail.com>
2020-08-26 14:10:16 +0530

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../../styles/components/Footer/footer.scss';
+
+const Footer = ()=> (
+  
+    <div className="footer">
+        <p className="desc">Rick and Morty is an American animated show that isn't Game of Thrones. It's a show where you watch a drunk Rick leading a socially insecure Morty into crazy interdimensional adventures and not to forget the absolute dumbfuck JERRY!</p>
+
+    </div>
+
+);
+
+export default Footer;
