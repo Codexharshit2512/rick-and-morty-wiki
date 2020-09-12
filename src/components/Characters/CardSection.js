44cb018a90ecc5dae5d129f91e06fad8303915c0
Harshit Jain <jain2008.harshu@gmail.com>
2020-09-12 19:52:29 +0530

    changed height of footer and added timeout to landing page also added link to characters section

--- src/components/Characters/CardSection.js
+++ src/components/Characters/CardSection.js
@@ -1,29 +1,45 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
 import Card from "./Card";
 import character from "./character";
 
-const CardSection = () => (
-  <div className="card-section container">
-    <h1
-      className="display-4 text-center"
-      style={{ color: "white", marginTop: 20 }}
-    >
-      Starring
-    </h1>
-    <div className="characters">
-      <div className="row">
-        {character.map((char) => (
-          <div className="col-sm" key={char.name}>
-            <Card
-              title={char.name}
-              description={char.description}
-              image={char.image}
-            />
-          </div>
-        ))}
+const CardSection = (props) => {
+  let history = useHistory();
+
+  return (
+    <div className="card-section container">
+      <h1
+        className="display-4 text-center"
+        style={{ color: "white", marginTop: 20 }}
+      >
+        Starring
+      </h1>
+      <div className="char-nav-link">
+        <div
+          className="icon-container"
+          onClick={() => history.push("/characters")}
+        >
+          <FontAwesomeIcon icon={faLongArrowAltRight} />
+          <div className="icon-bottom-border"></div>
+        </div>
+      </div>
+      <div className="characters">
+        <div className="row">
+          {character.map((char) => (
+            <div className="col-sm" key={char.name}>
+              <Card
+                title={char.name}
+                description={char.description}
+                image={char.image}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CardSection;
--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MainLanding from "../components/MainLanding/MainLanding";
 import CardSection from "../components/Characters/CardSection";
 import SeasonContainer from "../components/seasons/SeasonContainer";
 import MainCarousel from "../components/carousel/MainCarousel";
 import Hr from "../components/Hr";
+import Loader from "../components/loader/Loader";
 import Footer from "../components/Footer/Footer";
 
-const LandingPage = () => (
-  <>
-    <MainLanding />
-    <Hr />
-    <CardSection />
-    <Hr />
-    <SeasonContainer />
-    {/* <Hr /> */}
-    <MainCarousel />
-    {/* <Hr />
+const LandingPage = () => {
+  let [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 5000);
+  }, []);
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <MainLanding />
+          <Hr />
+          <CardSection />
+          <Hr />
+          <SeasonContainer />
+          {/* <Hr /> */}
+          <MainCarousel />
+          {/* <Hr />
     <DevSection /> */}
-    <Hr />
-    <Footer />
-  </>
-);
+          <Hr />
+          <Footer />
+        </>
+      )}
+    </>
+  );
+};
 
 export default LandingPage;
