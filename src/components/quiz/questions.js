962c499aa6841d4a671c52161009ff5b41a6f5f9
Mananx23 <manan.olympian23@gmail.com>
2020-10-06 12:40:47 +0530

    added questions

--- src/components/quiz/questions.js
+++ src/components/quiz/questions.js
@@ -21,7 +21,7 @@ export const questions = [
   {
     id: 4,
     question: "What is Beth's job",
-    options: ["Developer", "Cricketer", "Police", "Dalli"],
+    options: ["Developer", "Cricketer", "Police", "Surgeon"],
     answer: "Dalli",
   },
   {
@@ -29,5 +29,71 @@ export const questions = [
     question: "What is Rick's universe number",
     options: ["40", "30", "20", "10"],
     answer: "30",
+  },
+  {
+    id: 6,
+    question:
+      "What are the happy-go-lucky blue creatures called who inhabit planets across the universe?",
+    options: ["40", "30", "20", "10"],
+    answer: "Meeseeks",
+  },
+  {
+    id: 7,
+    question:
+      " Which game did Rick and Morty play in the final episode of season 3? ",
+    options: ["40", "30", "20", "10"],
+    answer: "Meeseeks",
+  },
+  {
+    id: 8,
+    question: " What is ‘Schmeckle’? ",
+    options: ["40", "30", "20", "10"],
+    answer: "Meeseeks",
+  },
+  {
+    id: 9,
+    question:
+      " In the episode Raising Gazorpazorp, what does Morty name his “child?” ",
+    options: ["40", "30", "20", "10"],
+    answer: "Meeseeks",
+  },
+  {
+    id: 10,
+    question: " What was Jerry Smith's major in college? ",
+    options: ["40", "30", "20", "10"],
+    answer: "Meeseeks",
+  },
+  {
+    id: 11,
+    question:
+      " Rick and Morty was one of the first shows to premiere a new episode on which social media platform? ",
+    options: ["40", "30", "20", "10"],
+    answer: "Meeseeks",
+  },
+  {
+    id: 12,
+    question:
+      " In the Anatomy Park episode, which theme park ride does Rick care about most? ",
+    options: ["40", "30", "20", "10"],
+    answer: "Meeseeks",
+  },
+  {
+    id: 13,
+    question: " What is the name of Rick's half-avian half-human best friend? ",
+    options: ["40", "30", "20", "10"],
+    answer: "Meeseeks",
+  },
+  {
+    id: 14,
+    question:
+      " Which fictional arcade in space does Rick drag Morty to in the episode Mortynight Run? ",
+    options: ["40", "30", "20", "10"],
+    answer: "Meeseeks",
+  },
+  {
+    id: 15,
+    question: "Which item helps Rick travel through the different universes?",
+    options: ["40", "30", "20", "10"],
+    answer: "Meeseeks",
   },
 ];
